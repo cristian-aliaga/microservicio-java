a2090f8b09a4391e771a7ca4d2fba959d2987381
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2024-05-12 18:46:40 -0700

    Prueba Blocker Sonarcloud

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
+	String passwd="12345";
 	public String greet() {
 		return "Hola, soy Cristian Aliaga";
-		String passwd="12345";
 	}
 }
